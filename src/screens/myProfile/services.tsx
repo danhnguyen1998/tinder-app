92d61064e6baa609c7641d6727b67bea7fb8df6b
danh.nguyenminh <danh.nguyenminh@powergatesoftware.com>
2020-09-11 10:45:58 +0700

    update code

--- src/screens/myProfile/services.tsx
+++ src/screens/myProfile/services.tsx
@@ -1,9 +1,9 @@
-import { configServices } from '@src/utils';
+import {configServices} from '@src/utils';
 
 export const getRandomUserService = async () => {
   try {
     const result = await configServices.getService('api/0.4', {randomapi: 0});
-    console.log(result, "1");
+    console.log(result, '1');
     return await configServices.getService('api/0.4', {randomapi: 0});
   } catch (error) {
     throw error;
--- src/screens/screens.reducers.tsx
+++ src/screens/screens.reducers.tsx
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux';
-import randomUserReducer from '@src/screens/myProfile/redux/reducers'
+import randomUserReducer from '@src/screens/myProfile/redux/reducers';
 
 export default combineReducers({
-  randomUserReducer
+  randomUserReducer,
 });
